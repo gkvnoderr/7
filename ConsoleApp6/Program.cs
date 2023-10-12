8adff4453932a8fdec655dc5d277dbed3a9b870b
gkvnoderr <123897793+gkvnoderr@users.noreply.github.com>
2023-10-12 15:53:31 +0300

    Add files via upload

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ////№1 Соответствие номера из таблицы Unicode символу
+            //for (int i = 0) ;
+            //Console.WriteLine($"Код {i} соотв.символу{(char)i}");
+            //№2 Дан символ. Вывести его код
+            //Console.WriteLine("Введите символ:");
+            //char ch = char.Parse(Console.ReadLine());
+            //Console.WriteLine($"Символ{ch})
+            //#3 Дан символ. Вывести символ, который в кодовой таблице ASCII следует за этим символом
+            //char d = Convert.ToChar(Console.ReadLine());
+            //int c = (int)d;
+            //c = 1;
+            //Console.WriteLine($"За символом {d} символ {(char)c}");
+            //Задача 5
+            //char d = Convert.ToChar(Console.ReadLine());
+            //int c = (int)d;
+            //c -= 1;
+            //int x = (int)d;
+            //x += 1;
+            //Задача 6. Дано название футбольного клуба. Напечатать его на экране «столбиком»
+            //Console.Write("Введите название клуба");
+            //string ch =Console.ReadLine();
+            //char[] omas = ch.ToCharArray();
+            //for (int i = 0; i < omas.Length; i++) 
+            //{
+
+            //    Console.WriteLine(omas[i]);
+
+            //}
+            Console.ReadKey();
+
+
+
+
+
+
+
+        }
+    }
+}
