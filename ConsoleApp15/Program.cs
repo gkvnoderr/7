41f8c9d1224ff10b7325b4384675adeb51eadd2b
gkvnoderr <123897793+gkvnoderr@users.noreply.github.com>
2023-10-16 08:26:39 +0300

    Add files via upload

---
+++ ConsoleApp15/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {   //Залание 1
+            //for (int i = 0; i < 256; i++)
+            //    Console.WriteLine($"код {i} соотвст к {(char)i}");
+
+            //задание 2
+            //Console.WriteLine("Введите символ: ");            
+            //char ch = char.Parse(Console.ReadLine());           
+            //Console.WriteLine("Символ: {0}; Код: {1}", ch, (int)ch);
+            //Console.ReadKey(true);
+
+            ////задание 3
+            //Console.Write("введ символ");
+            //char ch = char.Parse(Console.ReadLine());
+            //Console.WriteLine($"Символ : {(char)(int)(ch+1)}"); 
+
+            ////задание 4
+            //Console.WriteLine("введ симвл:  ");
+            //char ch = char.Parse(Console.ReadLine());
+            //Console.WriteLine($"Символы : {(char)(int)(ch - 1)} и {(char)(int)(ch-2)}");
+
+            ////задание 5
+            //Console.WriteLine("введ симвл:  ");
+            //char ch = char.Parse(Console.ReadLine());
+            //Console.WriteLine($"Символы : {(char)(int)(ch - 1)} и {(char)(int)(ch + 1)}");
+
+            ////задание 6
+            //Console.WriteLine("введ назван клуба");
+            //string name = Convert.ToString(Console.ReadLine());
+            //char[] omas = name.ToCharArray();
+            //for (int i=0; i<omas.Length; i++)
+            //{ Console.WriteLine(omas[i]);  }
+
+            ////задание 7
+            //Console.WriteLine("Введите слово: ");
+            //string name = Console.ReadLine();
+            //char[] chars = name.ToCharArray();
+            //for (int a = name.Length - 1; a >= 0; a--)
+            //{ Console.WriteLine($"{chars[a]}"); }
+
+            ////задание 8
+        //    Console.Write("Введите строку:");
+        //    string str = Console.ReadLine();
+        //    char[] simvol = str.ToCharArray();
+        //    for (int i = 0; i < simvol.Length; i++)
+        //    {
+        //        Console.Write(char.ToUpper(simvol[i]));
+        //    }
+        //    Console.WriteLine();
+        //    for (int i = 0; i < simvol.Length; i++)
+        //    {
+        //        Console.Write(char.ToLower(simvol[i]));
+        //    }
+
+
+
+
+        //    Console.ReadKey();
+        }
+    }
+}
